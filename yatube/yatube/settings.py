25518d749da0b58233cd2efc444ca14658e5584e
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-02 06:38:38 +0300

    Добавлен вывод изображений

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9p4$*-e8_*qnm&-b6=ob7#c08$1)&4bv8k5dsba#2!412d-(-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
     'about.apps.AboutConfig',
@@ -124,8 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = 'static/images'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -28,4 +28,8 @@ urlpatterns = [
     path('admin/myadmin/', admin.site.urls),
     path('about/', include('about.urls', namespace='about')),
     path("", include("posts.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
